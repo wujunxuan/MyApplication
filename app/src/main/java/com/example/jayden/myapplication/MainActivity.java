5fbdd1bce6f1a8c6c2cb81176c8cf163c70b97a9
jaydenwu <jaydenwu@tencent.com>
2019-06-27 15:32:28 +0800

    【新增】一些更新

--- app/src/main/java/com/example/jayden/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jayden/myapplication/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.jayden.myapplication;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.image_view);
 
+        textView = findViewById(R.id.text_view);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                String text = "imageWidth = " + imageView.getWidth()
+                        + ", imageHeight = " + imageView.getHeight()
+                        + ", bitmapWidth = " + bitmap.getWidth()
+                        + ", bitmapHeight = " + bitmap.getHeight()
+                        + ", size = " + (bitmap.getAllocationByteCount() / 1024f / 1024f);
+                textView.setText(text);
+            }
+        });
 
     }
 }
