49be93db698deefc8a8b474302dd8aea30f48a2c
jaydenwu <jaydenwu@tencent.com>
2019-06-27 16:54:21 +0800

    【新增】一些更新

--- app/src/main/java/com/example/jayden/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jayden/myapplication/MainActivity.java
@@ -19,23 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.image_view);
-
-        textView = findViewById(R.id.text_view);
-
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
-                Bitmap bitmap = bitmapDrawable.getBitmap();
-                String text = "imageWidth = " + imageView.getWidth()
-                        + ", imageHeight = " + imageView.getHeight()
-                        + ", bitmapWidth = " + bitmap.getWidth()
-                        + ", bitmapHeight = " + bitmap.getHeight()
-                        + ", size = " + (bitmap.getAllocationByteCount() / 1024f / 1024f);
-                textView.setText(text);
-            }
-        });
 
     }
 }
