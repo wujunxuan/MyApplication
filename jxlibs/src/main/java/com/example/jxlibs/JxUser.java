e97341146753cdb09004e0aff22c2af20371605c
jaydenwu <jaydenwu@tencent.com>
2019-06-27 16:52:39 +0800

    【新增】一些更新

---
+++ jxlibs/src/main/java/com/example/jxlibs/JxUser.java
@@ -0,0 +1,27 @@
+package com.example.jxlibs;
+
+/**
+ * Created by jaydenwu on 2019-06-27
+ * Description:
+ */
+public class JxUser {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
